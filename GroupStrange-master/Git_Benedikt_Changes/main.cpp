88b230d284f11bbf6cebaa55f98ecd7d6aa74541
Abbabara <34096779+Abbabara@users.noreply.github.com>
2017-11-30 22:11:15 +0000

    Add files via upload

---
+++ GroupStrange-master/Git_Benedikt_Changes/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Er thetta kodinn eda er eg ad baeta thessu vid kodann. Svor oskast ;-)" << endl;
+    cout << "virkar kallinn." << endl;
+    return 0;
+}
