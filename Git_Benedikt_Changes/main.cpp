5c6634f8151fd5519e3e1dec8adcf4dbc941a698
abbabara <arnbjorgf17@ru.is>
2017-12-01 10:21:39 +0000

    Frist comment

--- Git_Benedikt_Changes/main.cpp
+++ Git_Benedikt_Changes/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Er thetta kodinn eda er eg ad baeta thessu vid kodann. Svor oskast ;-)" << endl;
-    cout << "2222 Changes!!! Changes everywhere!!!"
+    cout << "2222 Changes!!! Changes everywhere!!!";
+    cout << "hehehehe";
     return 0;
 }
